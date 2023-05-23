9797432466ac37b5fc75d8a3e0a714f0e241524b
ehtesham-mirzakhani <ehtesham.mirzakhani@yahoo.com>
2023-05-23 09:54:38 +0330

    phase 4

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+def show(request):
+    return HttpResponse(request)
